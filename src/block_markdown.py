0799a2a0a5d621df3d820ac94bc4cc2b064a517b
avgra3 <antonygradillas@gmail.com>
2024-10-02 15:48:17 -0700

    Added block to block types with tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = []
@@ -5,3 +7,38 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         if block != "":
             blocks.append(block.strip())
     return blocks
+
+
+def block_to_block_type(block: str) -> str:
+    # Want to return a string representing
+    # the type of block it is.
+    # Assume all leading and trailing whitespace
+    # was already sripped
+    # HEADINGS: start with 1-5 "#" followed by a space then heading text
+    # CODE: start with 3 "`" and end with 3 "`"
+    # QUOTE: must start with ">"
+    # UNORDERED LIST: Must start with "*" or "-" followed by a space
+    # ORDERED LIST: Must start with a number followed by "." and space,
+    #   must start with 1 and increment by 1 for each line
+    # ELSE: normal paragraph
+    heading_regex = r"(^|\r|\n|\r\n)#{1,6} .*"
+    quote_regex = r"(^|\r|\n|\r\n)> .*"
+    unordered_list_regex = r"(^|\r|\n|\r\n)(\-|\*) .*"
+    ordered_list_regex = r"(^|\r|\n|\r\n)\d\. .*"
+    if re.search(heading_regex, block):
+        return "heading"
+    if block[:3] == "```" and block[-3:] == "```":
+        return "code"
+    if re.search(quote_regex, block):
+        return "quote"
+    if re.search(unordered_list_regex, block):
+        return "unordered_list"
+    if re.search(ordered_list_regex, block):
+        initial = 1
+        incremented = block.split("\n")
+        for line in incremented:
+            if initial != line[0]:
+                break
+            initial += 1
+        return "ordered_list"
+    return "paragraph"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -22,6 +22,80 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 * This is a list item
 * This is another list item"""]
         self.assertListEqual(markdown_to_blocks(markdown), expected)
+
+    def test_block_to_block_type_heading(self):
+        input1 = "# H1"
+        input2 = "## H2"
+        input3 = "###### H6"
+        expected1 = "heading"
+
+        input4 = "THIS SHOULD NOT MATCH ### "
+        input5 = "THIS SHOULD # NOT MATCH"
+        input6 = "#THIS SHOULD NOT MATCH"
+        input7 = "THIS SHOULD NOT MATCH"
+        expected2 = "paragraph"
+
+        self.assertEqual(block_to_block_type(input1), expected1)
+        self.assertEqual(block_to_block_type(input2), expected1)
+        self.assertEqual(block_to_block_type(input3), expected1)
+        self.assertEqual(block_to_block_type(input4), expected2)
+        self.assertEqual(block_to_block_type(input5), expected2)
+        self.assertEqual(block_to_block_type(input6), expected2)
+        self.assertEqual(block_to_block_type(input7), expected2)
+
+    def test_block_to_block_type_quote(self):
+        input1 = "> This should match"
+        expected1 = "quote"
+
+        input2 = "THIS SHOULD NOT MATCH > "
+        input3 = ">> THIS SHOULD NOT MATCH"
+        input4 = ">THIS SHOULD NOT MATCH"
+        input5 = "THIS SHOULD NOT MATCH"
+        expected2 = "paragraph"
+
+        self.assertEqual(block_to_block_type(input1), expected1)
+        self.assertEqual(block_to_block_type(input2), expected2)
+        self.assertEqual(block_to_block_type(input3), expected2)
+        self.assertEqual(block_to_block_type(input4), expected2)
+        self.assertEqual(block_to_block_type(input5), expected2)
+
+    def test_block_to_block_type_unordered_list(self):
+        input1 = """- This is part of a list\n- This is also part of a list\n- Item 3 in the list"""
+        input2 = """* This is part of a list\n* This is also part of a list\n* Item 3 in the list"""
+        expecetd1 = "unordered_list"
+
+        self.assertEqual(block_to_block_type(input1), expecetd1)
+        self.assertEqual(block_to_block_type(input2), expecetd1)
+
+        input3 = """-This is not an ordered list"""
+        input4 = """*This is not an ordered list"""
+        input5 = """* This is part of a list\n*This is not part of a list"""
+        expecetd2 = "paragraph"
+
+        self.assertEqual(block_to_block_type(input3), expecetd2)
+        self.assertEqual(block_to_block_type(input4), expecetd2)
+        self.assertEqual(block_to_block_type(input5), expecetd1)
+
+    def test_block_to_block_type_ordered_list(self):
+        input1 = """1. Should be found\n2. Should be found\n3. Should be found\n4. Should be found"""
+        expected1 = "ordered_list"
+
+        input2 = "SHOULD NOT BE FOUND"
+        input3 = "4.SHOULD NOT BE FOUND"
+        input4 = "SHOULD NOT BE FOUND 1. "
+        input5 = "SHOULD NOT 1. BE FOUND"
+        expected2 = "paragraph"
+
+        self.assertEqual(block_to_block_type(input1), expected1)
+        self.assertEqual(block_to_block_type(input2), expected2)
+        self.assertEqual(block_to_block_type(input3), expected2)
+        self.assertEqual(block_to_block_type(input4), expected2)
+        self.assertEqual(block_to_block_type(input5), expected2)
+
+    def test_block_to_block_type_paragraph(self):
+        input = "This is a paragraph!"
+        expecetd = "paragraph"
+        self.assertEqual(block_to_block_type(input), expecetd)
 
 
 if __name__ == "__main__":
