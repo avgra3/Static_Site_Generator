5c49aab61835f471d6a961cf809b720532e010e3
avgra3 <antonygradillas@gmail.com>
2024-10-03 14:05:53 -0700

    Added block markdown to html

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,26 +1,17 @@
 import re
+from htmlnode import LeafNode, ParentNode, HTMLNode
+from textnode import TextNode, text_node_to_html_node, text_type_text
 
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = []
     for block in markdown.split("\n\n"):
-        if block != "":
+        if block.strip() != "":
             blocks.append(block.strip())
     return blocks
 
 
 def block_to_block_type(block: str) -> str:
-    # Want to return a string representing
-    # the type of block it is.
-    # Assume all leading and trailing whitespace
-    # was already sripped
-    # HEADINGS: start with 1-5 "#" followed by a space then heading text
-    # CODE: start with 3 "`" and end with 3 "`"
-    # QUOTE: must start with ">"
-    # UNORDERED LIST: Must start with "*" or "-" followed by a space
-    # ORDERED LIST: Must start with a number followed by "." and space,
-    #   must start with 1 and increment by 1 for each line
-    # ELSE: normal paragraph
     heading_regex = r"(^|\r|\n|\r\n)#{1,6} .*"
     quote_regex = r"(^|\r|\n|\r\n)> .*"
     unordered_list_regex = r"(^|\r|\n|\r\n)(\-|\*) .*"
@@ -42,3 +33,44 @@ def block_to_block_type(block: str) -> str:
             initial += 1
         return "ordered_list"
     return "paragraph"
+
+
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    child_html_nodes = []
+    # Split markdown into blocks
+    markdown_blocks = markdown_to_blocks(markdown)
+    for block in markdown_blocks:
+        block_type = block_to_block_type(block)
+        if block_type == "quote":
+            leaf_node = LeafNode(
+                tag="blockquote", value=block.replace(">", "").strip(), )
+        if block_type == "paragraph":
+            leaf_node = LeafNode(tag="p", value=block,
+                                 props=None)
+        if block_type == "heading":
+            header_level = block.index(" ", 0)
+            leaf_node = LeafNode(
+                tag=f"h{header_level}", value=block.replace("#", "").strip())
+        if block_type == "unordered_list":
+            list_children = []
+            lines_of_block = block.split("\n")
+            for line in lines_of_block:
+                inner_leaf_node = LeafNode(
+                    tag="li", value=line.lstrip("* ").lstrip("- "))
+                list_children.append(inner_leaf_node)
+                leaf_node = ParentNode(tag="ul", children=list_children)
+        if block_type == "ordered_list":
+            list_children = []
+            lines_of_block = block.split("\n")
+            for line in lines_of_block:
+                inner_leaf_node = LeafNode(tag="li", value=line[2:].strip())
+                list_children.append(inner_leaf_node)
+                leaf_node = ParentNode(
+                    tag="ol", children=list_children)
+        if block_type == "code":
+            inner_html = LeafNode(
+                tag="code", value=block.replace("`", ""), props=None)
+            leaf_node = ParentNode(
+                tag="pre", children=[inner_html])
+        child_html_nodes.append(leaf_node)
+    return ParentNode(tag="div", children=child_html_nodes, props=None)
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from block_markdown import markdown_to_blocks, block_to_block_type
+from block_markdown import (markdown_to_blocks,
+                            block_to_block_type,
+                            markdown_to_html_node,
+                            )
+from htmlnode import ParentNode, LeafNode
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -97,6 +101,87 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         expecetd = "paragraph"
         self.assertEqual(block_to_block_type(input), expecetd)
 
+    def test_markdown_to_html_node_paragraph(self):
+        input = "This is a paragraph"
+        output = markdown_to_html_node(input)
+        expected = ParentNode(tag="div", children=[
+            LeafNode(tag="p", value=input)], props=None)
+        self.assertEqual(output.to_html(), expected.to_html())
+
+    def test_markdown_to_html_node_quote(self):
+        input = "```print('Hello world!')```"
+        output = markdown_to_html_node(input)
+        expected = ParentNode(tag="div", children=[ParentNode(tag="pre", children=[
+                              LeafNode(tag="code", value="print('Hello world!')")])])
+        self.assertEqual(
+            output.to_html(), expected.to_html())
+
+    def test_markdown_to_html_node_unordered_list(self):
+        input = "* This is a one item list\n* This is a second item list"
+        output = markdown_to_html_node(input)
+        children = [LeafNode(tag="li", value=input.split("\n")[0][2:]), LeafNode(
+            tag="li", value=input.split("\n")[1][2:])]
+        parent_inner = ParentNode(tag="ul", children=children)
+        expected = ParentNode(tag="div", children=[parent_inner])
+        self.assertEqual(output.to_html(), expected.to_html())
+
+    def test_markdown_to_html_node_ordered_list(self):
+        input = "1. This is a one item list\n2. This is a second item list"
+        output = markdown_to_html_node(input)
+        children = [LeafNode(tag="li", value="This is a one item list"), LeafNode(
+            tag="li", value="This is a second item list")]
+        parent_inner = ParentNode(tag="ol", children=children)
+        expected = ParentNode(tag="div", children=[parent_inner])
+        self.assertEqual(output.to_html(), expected.to_html())
+
+    def test_markdown_to_html_node_heading(self):
+        input1 = "# H1"
+        input2 = "## H2"
+        input3 = "### H3"
+        input4 = "#### H4"
+        input5 = "##### H5"
+        input6 = "###### H6"
+        expected1 = ParentNode(tag="div", children=[LeafNode(
+            tag="h1", value="H1")])
+        actual1 = markdown_to_html_node(input1)
+        expected2 = ParentNode(tag="div", children=[
+                               LeafNode(tag="h2", value="H2")])
+        actual2 = markdown_to_html_node(input2)
+        expected3 = ParentNode(tag="div", children=[
+                               LeafNode(tag="h3", value="H3")])
+        actual3 = markdown_to_html_node(input3)
+        expected4 = ParentNode(tag="div", children=[
+                               LeafNode(tag="h4", value="H4")])
+        actual4 = markdown_to_html_node(input4)
+        expected5 = ParentNode(tag="div", children=[
+                               LeafNode(tag="h5", value="H5")])
+        actual5 = markdown_to_html_node(input5)
+        expected6 = ParentNode(tag="div", children=[
+                               LeafNode(tag="h6", value="H6")])
+        actual6 = markdown_to_html_node(input6)
+
+        self.assertEqual(actual1.to_html(), expected1.to_html())
+        self.assertEqual(actual2.to_html(), expected2.to_html())
+        self.assertEqual(actual3.to_html(), expected3.to_html())
+        self.assertEqual(actual4.to_html(), expected4.to_html())
+        self.assertEqual(actual5.to_html(), expected5.to_html())
+        self.assertEqual(actual6.to_html(), expected6.to_html())
+
+    def test_markdown_to_html_mixed(self):
+        input = "# Header\n\n```print('hello world!')```\n\n1. Item one\n2. Item two\n\n* Unordered\n* Unordered\n\n> This is a quote"
+        actual = markdown_to_html_node(input)
+        inner_expected1 = LeafNode(tag="h1", value="Header")
+        inner_expected2 = ParentNode(
+            tag="pre", children=[LeafNode(tag="code", value="print('hello world!')")])
+        inner_expected3 = ParentNode(tag="ol", children=[LeafNode(
+            tag="li", value="Item one"), LeafNode(tag="li", value="Item two")])
+        inner_expected4 = ParentNode(tag="ul", children=[LeafNode(
+            tag="li", value="Unordered"), LeafNode(tag="li", value="Unordered")])
+        inner_expected5 = LeafNode(tag="blockquote", value="This is a quote")
+        expected = ParentNode(tag="div", children=[
+                              inner_expected1, inner_expected2, inner_expected3, inner_expected4, inner_expected5])
+        self.assertEqual(actual.to_html(), expected.to_html())
+
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
