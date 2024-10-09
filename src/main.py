73bc7a792eca91154fefb7fcb1587b3ddf41cbf8
avgra3 <antonygradillas@gmail.com>
2024-10-08 22:49:42 -0700

    Copy static content methods

--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
 from textnode import TextNode
+import os
+import shutil
+
+
+# Write a recursive function that copies all files from static dir to public dir
+# 1. Delete all contents of the destination directory to ensure that the copy is clean
+# 2. Copy all files and subdirectories, nested files, etc.
+# 3. Should log the path of each file copied to see what's happening
+def remove_old_static():
+    if os.path.exists(os.path.join(os.getcwd(), "public")):
+        shutil.rmtree(os.path.join(os.getcwd(), "public"))
+    os.mkdir(os.path.join(os.getcwd(), "public"))
+
+
+def copy_static(STATIC_BASE_DIR: str, PUBLIC_BASE_DIR: str):
+    items_in_dir = os.listdir(STATIC_BASE_DIR)
+    for item in items_in_dir:
+        static = os.path.join(STATIC_BASE_DIR, item)
+        copied = os.path.join(PUBLIC_BASE_DIR, item)
+        if os.path.isfile(static):
+            shutil.copy(src=static, dst=copied)
+            print(f"src={static} ==> dst={copied}")
+        if os.path.isdir(static):
+            # Make the directory if not exist
+            if not os.path.exists(copied):
+                os.mkdir(copied)
+                print(f"Directory made: {copied}")
+            copy_static(STATIC_BASE_DIR=static, PUBLIC_BASE_DIR=copied)
 
 
 def main():
-    dummy = TextNode(text="This is a text node",
-                     text_type="bold", url="https://www.boot.dev")
-
-    print(dummy)
+    remove_old_static()
+    copy_static(STATIC_BASE_DIR="static", PUBLIC_BASE_DIR="public")
 
 
 if __name__ == "__main__":
