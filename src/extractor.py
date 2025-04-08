369a5d1b3617c28777097dbb5e8966c850a445d3
avgra3 <antonygradillas@gmail.com>
2025-04-08 11:04:28 -0700

    Update for github pages

--- src/extractor.py
+++ src/extractor.py
@@ -11,7 +11,9 @@ def extract_title(markdown: str) -> str:
     raise Exception("There is no header!")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+    from_path: str, template_path: str, dest_path: str, basepath: str = "/"
+) -> None:
     message = f"Generating page {from_path} to {dest_path} using {template_path}"
     with open(from_path, "r") as file:
         markdown = file.read()
@@ -19,7 +21,12 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         template = file.read()
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    updated_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    updated_html = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
     dest_dirs = "".join(dest_path.split("/")[:-1])
     print(f"Dest path: {dest_path}")
     print(f"Dest dirs: {dest_dirs}")
@@ -33,7 +40,10 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
 
 def generate_pages_recursive(
-    dir_path_content: str, template_path: str, dest_dir_path: str
+    dir_path_content: str,
+    template_path: str,
+    dest_dir_path: str,
+    basepath: str = "/",
 ):
     items_in_dir = os.listdir(dir_path_content)
     for item in items_in_dir:
@@ -44,7 +54,10 @@ def generate_pages_recursive(
         if os.path.isfile(static) and static.endswith(".md"):
             message = f"Generating page {static} to {copied} using {template_path}"
             generate_page(
-                from_path=static, template_path=template_path, dest_path=copied
+                from_path=static,
+                template_path=template_path,
+                dest_path=copied,
+                basepath=basepath,
             )
         if os.path.isdir(static):
             if not os.path.exists(copied):
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from extractor import generate_pages_recursive
+import sys
 
 
 # Write a recursive function that copies all files from static dir to public dir
@@ -8,9 +9,10 @@ from extractor import generate_pages_recursive
 # 2. Copy all files and subdirectories, nested files, etc.
 # 3. Should log the path of each file copied to see what's happening
 def remove_old_static():
-    if os.path.exists(os.path.join(os.getcwd(), "public")):
-        shutil.rmtree(os.path.join(os.getcwd(), "public"))
-    os.mkdir(os.path.join(os.getcwd(), "public"))
+    buildDir = "docs"
+    if os.path.exists(os.path.join(os.getcwd(), buildDir)):
+        shutil.rmtree(os.path.join(os.getcwd(), buildDir))
+    os.mkdir(os.path.join(os.getcwd(), buildDir))
 
 
 def copy_static(STATIC_BASE_DIR: str, PUBLIC_BASE_DIR: str):
@@ -30,12 +32,19 @@ def copy_static(STATIC_BASE_DIR: str, PUBLIC_BASE_DIR: str):
 
 
 def main():
+    # Get the arguments passed on start
+    basepath = sys.argv[0]
+    if basepath == "":
+        basepath = "/"
+
+    buildDir = "docs"
+
     remove_old_static()
-    copy_static(STATIC_BASE_DIR="static", PUBLIC_BASE_DIR="public")
+    copy_static(STATIC_BASE_DIR="static", PUBLIC_BASE_DIR=buildDir)
     generate_pages_recursive(
         dir_path_content="content",
         template_path="template.html",
-        dest_dir_path="public",
+        dest_dir_path=buildDir,
     )
 
 
